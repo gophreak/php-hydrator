3f9e3638f8dea42c89837b2a51579c956a81d969
Raymond Jones <rjones@bluink.ca>
2026-07-29 18:55:46 -0400

    Refactored tests to use functional for Hydrator. Added simple tests for ArraySource so we have it tested as a unit

--- tests/Functional/HydratorTest.php
+++ tests/Functional/HydratorTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Tests\Unit;
+namespace Tests\Functional;
 
 use Hydrator\Exception\InvalidTypeException;
 use Hydrator\Exception\MissingValueException;
---
+++ tests/Unit/Sources/ArraySourceTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Sources;
+
+use Hydrator\Sources\ArraySource;
+use PHPUnit\Framework\TestCase;
+
+final class ArraySourceTest extends TestCase
+{
+    public function testHasReturnsCorrectly(): void
+    {
+        $arr = new ArraySource(['foo' => 'bar', 'baz' => 'qux', 'quux' => 'corge']);
+
+        $this->assertTrue($arr->has('foo'), 'foo should exist');
+        $this->assertTrue($arr->has('baz'), 'baz should exist');
+        $this->assertTrue($arr->has('quux'), 'quux should exist');
+        $this->assertFalse($arr->has('f'), 'f should not exist');
+    }
+
+    public function testGetReturnsCorrectly(): void
+    {
+        $arr = new ArraySource(['foo' => 'bar', 'baz' => 'qux', 'quux' => 'corge']);
+
+        $this->assertSame('bar', $arr->get('foo'));
+        $this->assertSame('qux', $arr->get('baz'));
+        $this->assertSame('corge', $arr->get('quux'));
+        $this->assertNull($arr->get('f'), 'f should return null');
+    }
+}
